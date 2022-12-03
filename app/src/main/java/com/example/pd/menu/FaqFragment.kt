b8c0c10bedb5432e454f0bea21885eaa78986ddb
jeromealapad13 <jeromealapad13@gmail.com>
2022-12-03 12:53:26 +0800

    Added Images and FAQ

--- app/src/main/java/com/example/pd/menu/FaqFragment.kt
+++ app/src/main/java/com/example/pd/menu/FaqFragment.kt
@@ -7,12 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.pd.R
 
-class SettingsFragment : Fragment() {
+class FaqFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        return inflater.inflate(R.layout.fragment_faq, container, false)
     }
 }
