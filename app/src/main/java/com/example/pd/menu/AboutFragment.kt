877b9f181d3dbd3559845d7180143456abe2524f
Kierwin Jov Ramos <qkjpramos@tip.edu.ph>
2022-12-16 20:34:41 +0800

    Bottom Navbar fixed
    rev2
    - Bottom NavBar fixed
    - Navbar icons added
    issues:
    - Signal API in navigation menus not opening

--- app/src/main/java/com/example/pd/menu/AboutFragment.kt
+++ app/src/main/java/com/example/pd/menu/AboutFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toolbar
 import com.example.pd.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class AboutFragment : Fragment() {
     override fun onCreateView(
@@ -14,5 +16,13 @@ class AboutFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val bottomNavigationView = activity?.findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        bottomNavigationView?.visibility = View.VISIBLE
+
     }
 }
